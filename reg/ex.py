46acc63568fcd15c9470c70eeb9729548e55c486
Sarper Ekinci <sarperekinci@hotmail.com>
2019-07-19 21:07:39 +0300

    update

--- reg/ex.py
+++ reg/ex.py
@@ -8,21 +8,7 @@ import utils
 from PIL import Image
 
 
-# idx_to_name = {  
-#                  3: 'Sarper Ekinci',
-#                  2: 'hediye',
-#                  1: 'Kaan',
-#                 -1: 'Unknown',
-# }
-# r.register(2, 'database/Hediye_Parlar')
-# r.register(3, 'database/sarper_ekinci')
 
-# image_path = img
-# bboxs, idx = r(image_path)
-# names = [idx_to_name[i] for i in idx]
-# image = Image.open(image_path)
-# utils.draw(image, bboxs, names)
-# image.save('deneme.jpg')
 def setInitials(data,img):
     detection_params_path = 'detection_params.json'
     recognization_params_path = 'recognization_params.json'
